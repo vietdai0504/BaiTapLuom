5b202be4f3d01bb29c735965b065363660003351
vietdai0504 <daithai.05042005@gmail.com>
2024-03-29 19:10:18 +0700

    add project Changes to be committed:

---
+++ XoanOc/xoanOc.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <fstream>
+#include <iomanip>
+using namespace std;
+int A[100][100] = {0};
+void printA(int A[][100], int N, ofstream& file)
+{
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			file <<setw(2)<< A[i][j] << " ";
+		}
+		file << endl;
+	}
+}
+void xoanOc(int A[][100], int N, int d)
+{
+	int value = 1;
+	int collumnS = 0, rowS = N - 1, collumnE = N - 1, rowE = 0;
+	while (collumnS <= collumnE && rowE <= rowS)
+	{
+		for (int i = collumnS; i <= collumnE; i++)
+		{
+			A[rowS][i] = value;
+			value += d;
+		}
+		rowS--;
+		for (int i = rowS; i >= rowE; i--)
+		{
+			A[i][collumnE] = value;
+			value += d;
+		}
+		collumnE--;
+		for (int i = collumnE; i >= collumnS; i--)
+		{
+			A[rowE][i] = value;
+			value += d;
+		}
+		rowE++;
+		for (int i = rowE; i <= rowS; i++)
+		{
+			A[i][collumnS] = value;
+			value += d;
+		}
+		collumnS++;
+	}
+}
+int main()
+{
+	ifstream fin("XoanOc.inp");
+	ofstream fout("XoanOc.out");
+	int N, d; fin >> N >> d;
+	xoanOc(A, N, d);
+	printA(A, N,fout);
+}
